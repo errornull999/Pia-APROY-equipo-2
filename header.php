b5a5dea6e7b6de756eb805a2874ba89cb2fe46ec
Isaid Aguiar <isaid.aguiar@gmail.com>
2026-05-11 17:40:12 -0600

    Cambios en el diseño

--- header.php
+++ header.php
@@ -9,6 +9,9 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Solace - Sesiones Fotográficas</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&family=Playfair+Display:ital,wght@0,600;1,600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
