a28f04cc317bc4782301b1c36d1939b700d9a625
Isaid Aguiar <isaid.aguiar@gmail.com>
2026-05-11 21:31:26 -0600

    cambio en el pie de pagina

--- footer.php
+++ footer.php
@@ -1,31 +1,16 @@
-</div>
+</div> 
 
-    <footer style="text-align: center; padding: 30px; color: white; margin-top: 50px;">
-        <div style="margin-bottom: 10px; ">
-            <a href="https://www.instagram.com/solacebygey?igsh=MWVtdWlqa242Nm1lcA==" target="_blank" style="margin: 0 15px; display: inline-block; transition: transform 0.3s ease;">
-                <img src="img/insta-logo.png" alt="Instagram" width="10px" height="10px" style="filter: brightness(0) invert(1);">
+<footer class="main-footer">
+        <div class="social-links">
+            <a href="https://www.instagram.com/solacebygey?igsh=MWVtdWlqa242Nm1lcA==" target="_blank">
+                <img src="img/insta-logo.png" alt="Instagram">
             </a>
-            <a href="https://www.facebook.com/share/1CKcoN93in/" target="_blank" style="margin: 0 15px; display: inline-block; transition: transform 0.3s ease;">
-                <img src="img/face.png" alt="Facebook" width="10px" height="10px" style="filter: brightness(0) invert(1);">
+            <a href="https://www.facebook.com/share/1CKcoN93in/" target="_blank">
+                <img src="img/face.png" alt="Facebook">
             </a>
         </div>
         <p>&copy; <?php echo date('Y'); ?> Solace - Capturando tus mejores momentos</p>
-    </footer>
-         <style>
-        img{
-            height: 40px;
-            width: 40px;
-        }
-        footer a:hover {
-            transform: scale(1.1);
-        }
-        footer a img {
-            transition: opacity 0.3s ease;
-        }
-        footer a:hover img {
-            opacity: 0.8;
-        }
-    </style>
+</footer>
    
 </body>
 </html>
